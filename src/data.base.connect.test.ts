62eb6d13424d4ac1b52e7f58ec8a7341e13928a1
Liluminalia <peloazul4@gmail.com>
2022-11-29 11:56:40 +0100

    adding data base conection test

---
+++ src/data.base.connect.test.ts
@@ -0,0 +1,25 @@
+import { dataBaseConnect } from './data.base.connect.js';
+import mongoose from 'mongoose';
+const spiConnect = jest.spyOn(mongoose, 'connect');
+describe('given dataBaseConnect', () => {
+    describe('when we call it', () => {
+        test('then it should get connected to the data base', async () => {
+            const result = await dataBaseConnect();
+            expect(spiConnect).toHaveBeenCalled();
+            expect(typeof result).toBe(typeof mongoose);
+            expect(result.connection.db.databaseName).toBe('CodersTesting');
+
+            mongoose.disconnect();
+        });
+    });
+    describe('when we call it', () => {
+        test('then it should get connected to the data base', async () => {
+            process.env.NODE_ENV = 'finalProyectData';
+            const result = await dataBaseConnect();
+            expect(spiConnect).toHaveBeenCalled();
+            expect(typeof result).toBe(typeof mongoose);
+            expect(result.connection.db.databaseName).toBe('finalProjectData');
+            mongoose.disconnect();
+        });
+    });
+});
