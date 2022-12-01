b2e2f5fc59db5ba9ed0ac0af9759f44b0e3add72
Liluminalia <peloazul4@gmail.com>
2022-12-01 15:32:17 +0100

    updating repo to get what i really need

--- src/repository/repo.ts
+++ src/repository/repo.ts
@@ -3,14 +3,18 @@ export interface BasicRepo<T> {
     get: (id: id) => Promise<T>;
     getAll: () => Promise<Array<T>>;
     post: (data: Partial<T>) => Promise<T>;
-    find: (data: any) => Promise<T>;
     patch: (id: id, data: Partial<T>) => Promise<T>;
 }
 
 export interface ExtraRepo<T> {
+    find: (data: any) => Promise<T>;
+}
+export interface FullRepo<T> {
+    get: (id: id) => Promise<T>;
     getAll: () => Promise<Array<T>>;
+    post: (data: Partial<T>) => Promise<T>;
     patch: (id: id, data: Partial<T>) => Promise<T>;
-    delete: (id: id) => Promise<{ id: id }>;
+    find: (data: any) => Promise<T>;
 }
 
 export interface Repo<T> extends BasicRepo<T> {
