df759403bebd74ccf2dbec47fbae646973b61d3f
Liluminalia <peloazul4@gmail.com>
2022-12-04 22:20:39 +0100

    fixing more duplications for sonar

--- src/middlewares/interceptor.test.ts
+++ src/middlewares/interceptor.test.ts
@@ -1,166 +1,177 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserRepository } from '../repository/user.repository';
 import { admin, authentication, ExtraRequest, logged } from './interceptor';
-describe('given logged function', () => {
-    describe('when authorization is ok ', () => {
-        test('then should pass to the next function', () => {
-            const req: Partial<ExtraRequest> = {
-                get: jest
-                    .fn()
-                    .mockReturnValueOnce(
-                        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODcyZjJkZDZmZmFiMDRkOTgxNTM0NCIsIm5hbWUiOiJhZHJpYW5hU2FsbGVzIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjcwMDAxOTUxfQ.9v54DWN5tAjxjrzb-leNzQSPEL9t-jceAzX5h88-z1s'
-                    ),
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
 
-            logged(req as ExtraRequest, res as Response, next);
-            expect(next).toHaveBeenCalled();
+describe('Given the interceptor', () => {
+    describe('given logged function', () => {
+        describe('when authorization is ok ', () => {
+            test('then should pass to the next function', () => {
+                const req: Partial<ExtraRequest> = {
+                    get: jest
+                        .fn()
+                        .mockReturnValueOnce(
+                            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODcyZjJkZDZmZmFiMDRkOTgxNTM0NCIsIm5hbWUiOiJhZHJpYW5hU2FsbGVzIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjcwMDAxOTUxfQ.9v54DWN5tAjxjrzb-leNzQSPEL9t-jceAzX5h88-z1s'
+                        ),
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
 
-            expect(req.payload).toStrictEqual({
-                id: expect.any(String),
-                iat: expect.any(Number),
-                name: 'adrianaSalles',
-                role: 'admin',
+                logged(req as ExtraRequest, res as Response, next);
+                expect(next).toHaveBeenCalled();
+
+                expect(req.payload).toStrictEqual({
+                    id: expect.any(String),
+                    iat: expect.any(Number),
+                    name: 'adrianaSalles',
+                    role: 'admin',
+                });
+            });
+        });
+        describe('when the authorization is not ok ', () => {
+            test('then if the authString is empty, it should return an error', () => {
+                const req: Partial<Request> = {
+                    get: jest.fn().mockReturnValueOnce(false),
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
+
+                logged(req as Request, res as Response, next);
+                expect(next).toHaveBeenCalled();
+            });
+            test('Then if the token its not valid, it should return an error', () => {
+                const req: Partial<Request> = {
+                    get: jest.fn().mockReturnValueOnce('Bearer token'),
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
+
+                logged(req as Request, res as Response, next);
+                expect(next).toHaveBeenCalled();
             });
         });
     });
-    describe('when the authorization is not ok ', () => {
-        test('then if the authString is empty, it should return an error', () => {
-            const req: Partial<Request> = {
-                get: jest.fn().mockReturnValueOnce(false),
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
+    describe('Given authentication function', () => {
+        describe('When the payload.id is equal to userId', () => {
+            const userRepository = UserRepository.getInstance();
+            test('then it should call function next', async () => {
+                const req: Partial<ExtraRequest> = {
+                    payload: {
+                        name: 'adrianaSalles',
+                        id: '63872f2dd6ffab04d9815344',
+                        role: 'admin',
+                    },
+                };
+                userRepository.get = jest
+                    .fn()
+                    .mockResolvedValue({ id: '63872f2dd6ffab04d9815344' });
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
 
-            logged(req as Request, res as Response, next);
-            expect(next).toHaveBeenCalled();
+                await authentication(
+                    req as ExtraRequest,
+                    res as Response,
+                    next
+                );
+                expect(next).toHaveBeenCalled();
+            });
         });
-        test('Then if the token its not valid, it should return an error', () => {
-            const req: Partial<Request> = {
-                get: jest.fn().mockReturnValueOnce('Bearer token'),
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
+        describe('When payload.id is different that userId', () => {
+            const userRepository = UserRepository.getInstance();
+            test('then should call next function and throw an error', async () => {
+                const req: Partial<ExtraRequest> = {
+                    payload: {
+                        name: 'adrianaSalles',
+                        id: '63872f2dd6ffab04d9815344',
+                        role: 'admin',
+                    },
+                };
+                userRepository.get = jest
+                    .fn()
+                    .mockResolvedValue({ id: '63872f2dd6ffab5559815344' });
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
 
-            logged(req as Request, res as Response, next);
-            expect(next).toHaveBeenCalled();
+                await authentication(
+                    req as ExtraRequest,
+                    res as Response,
+                    next
+                );
+                expect(next).toHaveBeenCalled();
+            });
+        });
+        describe('When the payload is not ok', () => {
+            test('then it will throw an error', () => {
+                const req: Partial<ExtraRequest> = {
+                    payload: undefined,
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
+                const error = new Error('usuario o contraseña incorrectos');
+                authentication(req as ExtraRequest, res as Response, next);
+                expect(error).toBeInstanceOf(Error);
+            });
         });
     });
-});
-describe('Given authentication function', () => {
-    describe('When the payload.id is equal to userId', () => {
+    describe('Given admin function', () => {
         const userRepository = UserRepository.getInstance();
-        test('then it should call function next', async () => {
-            const req: Partial<ExtraRequest> = {
-                payload: {
-                    name: 'adrianaSalles',
+        describe('When payload is not ok', () => {
+            test('Then it should throw an error', () => {
+                const user = {
                     id: '63872f2dd6ffab04d9815344',
                     role: 'admin',
-                },
-            };
-            userRepository.get = jest
-                .fn()
-                .mockResolvedValue({ id: '63872f2dd6ffab04d9815344' });
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
-
-            await authentication(req as ExtraRequest, res as Response, next);
-            expect(next).toHaveBeenCalled();
+                };
+                userRepository.get = jest.fn().mockResolvedValue(user);
+                const req: Partial<ExtraRequest> = {
+                    payload: undefined,
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
+                const error = new Error('usuario o contraseña incorrectos');
+                admin(req as ExtraRequest, res as Response, next);
+                expect(error).toBeInstanceOf(Error);
+            });
         });
-    });
-    describe('When payload.id is different that userId', () => {
-        const userRepository = UserRepository.getInstance();
-        test('then should call next function and throw an error', async () => {
-            const req: Partial<ExtraRequest> = {
-                payload: {
-                    name: 'adrianaSalles',
+        describe('When role is admin', () => {
+            test('then should have been called', () => {
+                const user = {
                     id: '63872f2dd6ffab04d9815344',
                     role: 'admin',
-                },
-            };
-            userRepository.get = jest
-                .fn()
-                .mockResolvedValue({ id: '63872f2dd6ffab5559815344' });
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
+                };
+                userRepository.get = jest.fn().mockResolvedValue(user);
+                const req: Partial<ExtraRequest> = {
+                    payload: {
+                        name: 'adrianaSalles',
+                        id: '63872f2dd6ffab04d9815344',
+                        role: 'admin',
+                    },
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
 
-            await authentication(req as ExtraRequest, res as Response, next);
-            expect(next).toHaveBeenCalled();
+                admin(req as ExtraRequest, res as Response, next);
+                expect(user.role && req.payload!.role).toBe('admin');
+            });
         });
-    });
-    describe('When the payload is not ok', () => {
-        test('then it will throw an error', () => {
-            const req: Partial<ExtraRequest> = {
-                payload: undefined,
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
-            const error = new Error('usuario o contraseña incorrectos');
-            authentication(req as ExtraRequest, res as Response, next);
-            expect(error).toBeInstanceOf(Error);
-        });
-    });
-});
-describe('Given admin function', () => {
-    const userRepository = UserRepository.getInstance();
-    describe('When payload is not ok', () => {
-        test('Then it should throw an error', () => {
-            const user = {
-                id: '63872f2dd6ffab04d9815344',
-                role: 'admin',
-            };
-            userRepository.get = jest.fn().mockResolvedValue(user);
-            const req: Partial<ExtraRequest> = {
-                payload: undefined,
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
-            const error = new Error('usuario o contraseña incorrectos');
-            admin(req as ExtraRequest, res as Response, next);
-            expect(error).toBeInstanceOf(Error);
-        });
-    });
-    describe('When role is admin', () => {
-        test('then should have been called', () => {
-            const user = {
-                id: '63872f2dd6ffab04d9815344',
-                role: 'admin',
-            };
-            userRepository.get = jest.fn().mockResolvedValue(user);
-            const req: Partial<ExtraRequest> = {
-                payload: {
-                    name: 'adrianaSalles',
-                    id: '63872f2dd6ffab04d9815344',
-                    role: 'admin',
-                },
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
-
-            admin(req as ExtraRequest, res as Response, next);
-            expect(user.role && req.payload!.role).toBe('admin');
-        });
-    });
-    describe('When the role is not admin', () => {
-        test('Then its going to throw error', () => {
-            const user = {
-                id: '63872f2dd6ffab04d9815344',
-                role: 'user',
-            };
-            userRepository.get = jest.fn().mockResolvedValue(user);
-            const req: Partial<ExtraRequest> = {
-                payload: {
-                    name: 'adrianaSalles',
+        describe('When the role is not admin', () => {
+            test('Then its going to throw error', () => {
+                const user = {
                     id: '63872f2dd6ffab04d9815344',
                     role: 'user',
-                },
-            };
-            const res: Partial<Response> = {};
-            const next: NextFunction = jest.fn();
-            const error = new Error('usuario o contraseña incorrectos');
-            admin(req as ExtraRequest, res as Response, next);
-            expect(error).toBeInstanceOf(Error);
+                };
+                userRepository.get = jest.fn().mockResolvedValue(user);
+                const req: Partial<ExtraRequest> = {
+                    payload: {
+                        name: 'adrianaSalles',
+                        id: '63872f2dd6ffab04d9815344',
+                        role: 'user',
+                    },
+                };
+                const res: Partial<Response> = {};
+                const next: NextFunction = jest.fn();
+                const error = new Error('usuario o contraseña incorrectos');
+                admin(req as ExtraRequest, res as Response, next);
+                expect(error).toBeInstanceOf(Error);
+            });
         });
     });
 });
